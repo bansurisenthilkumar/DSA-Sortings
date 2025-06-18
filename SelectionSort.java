5b43cbd628c084fb74367b06fe650a8a47b9c74d
Bansuri Senthilkumar <74858569+bansurisenthilkumar@users.noreply.github.com>
2025-06-18 21:04:37 +0530

    Create SelectionSort.java

---
+++ SelectionSort.java
@@ -0,0 +1,27 @@
+package Sorting;
+
+public class SlectionPrac {
+
+	public static void main(String[] args) {
+		
+		int[] arr = {90,100,70,80,60,110};
+		int n = arr.length;
+		for(int i=0;i<n-2;i++) {
+			int min = i;
+			for(int j=i;j<=n-1;j++) {
+				if(arr[j]<arr[min]) {
+					min = j;
+				}
+			}
+			int temp = arr[i];
+			arr[i] = arr[min];
+			arr[min] = temp;
+		}
+		
+		for(int k=0;k<=n-1;k++) {
+			System.out.print(arr[k]+" ");
+		}
+
+	}
+
+}
